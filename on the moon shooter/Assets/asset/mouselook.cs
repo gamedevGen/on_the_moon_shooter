465747102565d81071371b55bf8a104a2748f7a3
gamedevGen <73786407+gamedevGen@users.noreply.github.com>
2020-11-02 16:41:43 +0200

    the givty change

---
+++ on the moon shooter/Assets/asset/mouselook.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouselook : MonoBehaviour
+{
+    public float mousesensitivity = 100f;
+
+    public Transform playerbody;
+
+    float xRotation = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mousesensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mousesensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerbody.Rotate(Vector3.up * mouseX);
+
+
+    }
+}
---
+++ on the moon shooter/Assets/asset/playrmovemit.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playrmovemit : MonoBehaviour
+{
+    public CharacterController contterol;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public Transform groundcack;
+    public float groundfar = 0.4f;
+    public LayerMask groundmask;
+    bool isgrounded;
+    public float jumpHeight = 3f;
+
+
+    Vector3 velocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isgrounded = Physics.CheckSphere(groundcack.position, groundfar, groundmask);
+        if (isgrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        
+        }
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        if(Input.GetButtonDown("Jump") && isgrounded)
+        {
+
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+        contterol.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        contterol.Move(velocity * Time.deltaTime);
+
+    }
+}
