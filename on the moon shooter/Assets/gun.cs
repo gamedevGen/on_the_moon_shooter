d03e8724f44108b3692f9e529bb537d553645b9c
gamedevGen <73786407+gamedevGen@users.noreply.github.com>
2020-11-03 14:19:29 +0200

    the real shooter

---
+++ on the moon shooter/Assets/gun.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+public class gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera fpscam;
+    public ParticleSystem laserShoot;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            shoot();
+        }
+
+        
+    }
+
+    void shoot ()
+    {
+        laserShoot.Play();
+        RaycastHit hi;
+        if (Physics.Raycast(fpscam.transform.position, fpscam.transform.forward, out hi, range))
+        {
+            Debug.Log(hi.transform.name);
+           target Target = hi.transform.GetComponent<target>();
+            if (Target != null)
+            {
+                Target.Takedamge(damage);
+            }
+        }
+    }
+}
---
+++ on the moon shooter/Assets/target.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Linq.Expressions;
+using UnityEngine;
+
+public class target : MonoBehaviour
+{
+    public float heath = 25f;
+
+    public void Takedamge (float amount)
+    {
+        heath -= amount;
+        if(heath <= 0f)
+        {
+            die();
+        }
+    }
+    void die()
+    {
+        Destroy(gameObject);
+    }
+}
