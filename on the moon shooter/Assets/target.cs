6d01abe7a36030a27bd2d77c07225461fc8f0a73
gamedevGen <73786407+gamedevGen@users.noreply.github.com>
2020-11-04 08:19:18 +0200

    the fix

--- on the moon shooter/Assets/target.cs
+++ on the moon shooter/Assets/target.cs
@@ -6,17 +6,17 @@ using UnityEngine;
 public class target : MonoBehaviour
 {
     public float heath = 25f;
-<<<<<<< HEAD
-    public ParticleSystem dead;
+
+    
     float delay = 1.6f;
     bool effectPlayed = false;
     private void Start()
     {
-        dead.Stop();
+       
     }
-=======
 
->>>>>>> parent of 5c6e23a... effects
+
+
     public void Takedamge (float amount)
     {
         heath -= amount;
@@ -30,17 +30,10 @@ public class target : MonoBehaviour
     }
     void die()
     {
-<<<<<<< HEAD
-        dead.Play();
-        StartCoroutine((string)effectDelay());
+
         
-=======
->>>>>>> parent of 5c6e23a... effects
+
         Destroy(gameObject);
     }
-    IEnumerable effectDelay()
-    {
-        yield return new WaitForSeconds(1.8f);
 
-    }
 }
