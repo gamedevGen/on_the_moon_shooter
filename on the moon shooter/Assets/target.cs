a4583ed779d3b93b1a7ce455124b61c92e957999
gamedevGen <73786407+gamedevGen@users.noreply.github.com>
2020-11-04 07:52:52 +0200

    Update target.cs

--- on the moon shooter/Assets/target.cs
+++ on the moon shooter/Assets/target.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class target : MonoBehaviour
 {
     public float heath = 25f;
-<<<<<<< HEAD
+
     public ParticleSystem dead;
     float delay = 1.6f;
     bool effectPlayed = false;
@@ -14,9 +14,7 @@ public class target : MonoBehaviour
     {
         dead.Stop();
     }
-=======
 
->>>>>>> parent of 5c6e23a... effects
     public void Takedamge (float amount)
     {
         heath -= amount;
@@ -30,12 +28,11 @@ public class target : MonoBehaviour
     }
     void die()
     {
-<<<<<<< HEAD
+
         dead.Play();
         StartCoroutine((string)effectDelay());
         
-=======
->>>>>>> parent of 5c6e23a... effects
+
         Destroy(gameObject);
     }
     IEnumerable effectDelay()
